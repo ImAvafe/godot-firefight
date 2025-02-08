26211631d73d142c117159dd5482e934c5a5af64
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-02-08 14:47:51 -0500

    Add tree perishing

--- scenes/tree/tree.gd
+++ scenes/tree/tree.gd
@@ -9,6 +9,10 @@ extends Node2D
 func _ready() -> void:
 	burning = randi() % 3 == 2
 
+	$BurnTimer.timeout.connect(func():
+		perish()
+	)
+
 
 func _process(_delta: float) -> void:
 	pass
@@ -18,6 +22,30 @@ func update_burning():
 	$Sprite2D/Fire.visible = burning
 	
 	if burning:
+		$BurnTimer.start()
 		$Sprite2D/Fire.play()
 	else:
+		$BurnTimer.stop()
 		$Sprite2D/Fire.stop()
+
+
+func smoke():
+	$Sprite2D/Smoke.visible = true
+	$Sprite2D/Smoke.play()
+
+
+func perish():
+	burning = false
+	smoke()
+
+	var tween = get_tree().create_tween()
+	tween.tween_property($Sprite2D, "scale", Vector2.ZERO, 0.2)
+	tween.tween_callback(func():
+		queue_free()
+	)
+	tween.play()
+
+
+func extinguish():
+	burning = false
+	smoke()
