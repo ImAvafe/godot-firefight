753c313b76abfeb5c108ff35d0e144a547a11986
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-02-09 15:21:30 -0500

    Add title screen

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,6 +3,7 @@ extends Node
 const fireball = preload("res://scenes/fireball/fireball.tscn")
 
 var game_active = false
+var game_over = false
 var game_started = null
 var level = 0
 var fireball_instance
@@ -16,28 +17,44 @@ func _process(_delta: float):
     fireball_instance.speed = speed
 
   if Input.is_action_just_pressed("start_game"):
-    if not game_active:
+    if game_over:
+      reset_game()
+    else:
       start_game()
 
 
 func start_game():
   game_active = true
+  game_over = false
   game_started = Time.get_unix_time_from_system()
   level = 0
 
+  $TitleScreen.hide()
   $GameOverScreen.hide()
-  $Map/Map/Score.show()
+  $Map/Score.show()
 
   new_level()
 
 
 func end_game():
   game_active = false
+  game_over = true
+
   fireball_instance.queue_free()
 
   $GameOverScreen/VBoxContainer/Score.text = "SCORE: " + str(round(Time.get_unix_time_from_system() - game_started))
   $GameOverScreen.show()
-  $Map/Map/Score.hide()
+  $Map/Score.hide()
+
+
+func reset_game():
+  game_over = false
+  game_active = false
+
+  $Map.despawn_trees()
+
+  $TitleScreen.show()
+  $GameOverScreen.hide()
 
 
 func new_level():
